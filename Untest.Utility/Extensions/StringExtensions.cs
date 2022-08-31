d8033d7801739b62571e8d16932427084d4d5e4b
Yoga <yoga@hotaimotor.com.tw>
2022-08-31 17:29:29 +0800

    [單元測試]使用NUnit，測試public class數字轉字串_預期取得字串型別的整數

---
+++ Untest.Utility/Extensions/StringExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Untest.Utility.Extensions
+{
+    public class StringExtensions
+    {
+        /// <summary>
+        /// 數字轉字串，取到小數點第0位
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public string ToCurrencyString(object input)
+        {
+            var isNumber = input != null ? Regex.IsMatch(input.ToString(), @"\d") : false;
+            return isNumber ? string.Format("{0:N0}", input) : "0";
+        }
+
+        //string Formatting of 1054.32179:
+        //          N:                     1,054.32 
+        //          N0:                    1,054 
+        //          N1:                    1,054.3 
+        //          N2:                    1,054.32 
+        //          N3:                    1,054.322 
+
+    }
+}
---
+++ Untest.UtilityTests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+using Untest.Utility.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Untest.Utility.Extensions.Tests
+{
+    [TestFixture()]
+    public class StringExtensionsTests
+    {
+        [Test(Description= "Test_ToCurrencyString_數字小數點第1位_預期取得字串型別的整數")]
+        public void Test_ToCurrencyString_數字小數點第1位_預期取得字串型別的整數()
+        {
+            //arrange--------------------------------------------
+            #region 直接 new 出實體，未隔離
+            //注：此為public class，not static class，所以未使用DI隔離class
+            #endregion
+            var stringExtensions = new StringExtensions();    
+            var sut = 123.4;       //被測物
+            var expected = "123";  //預期的結果
+
+            //act-------------------------------------------------
+            var result = stringExtensions.ToCurrencyString(sut);
+
+            //assert--------------------------------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        #region 同個method，傳入多個sut
+        //[Test(Description = "Test_ToCurrencyString_數字小數點第2位_預期取得字串型別的整數")]
+        //public void Test_ToCurrencyString_數字小數點第2位_預期取得字串型別的整數()
+        //{
+        //    //arrange--------------------------------------------          
+        //    var stringExtensions = new StringExtensions();
+        //    var sut = 123.45;       //被測物
+        //    var expected = "123";  //預期的結果
+
+        //    //act-------------------------------------------------
+        //    var result = stringExtensions.ToCurrencyString(sut);
+
+        //    //assert--------------------------------------------
+        //    Assert.AreEqual(expected, result);
+        //}
+
+        //[Test(Description = "Test_ToCurrencyString_數字小數點第3位_預期取得字串型別的整數")]
+        //public void Test_ToCurrencyString_數字小數點第3位_預期取得字串型別的整數()
+        //{
+        //    //arrange--------------------------------------------          
+        //    var stringExtensions = new StringExtensions();
+        //    var sut = 123.456;       //被測物
+        //    var expected = "123";  //預期的結果
+
+        //    //act-------------------------------------------------
+        //    var result = stringExtensions.ToCurrencyString(sut);
+
+        //    //assert--------------------------------------------
+        //    Assert.AreEqual(expected, result);
+        //}
+        #endregion
+
+        #region 測試失敗
+        //[Test(Description = "Test_ToCurrencyString_數字小數點第4位_預期取得字串型別的整數")]
+        //public void Test_ToCurrencyString_數字小數點第4位_預期取得字串型別的整數()
+        //{
+        //    //arrange--------------------------------------------          
+        //    var stringExtensions = new StringExtensions();
+        //    var sut = 123.4567;       //被測物
+        //    var expected = "123.";  //預期的結果
+
+        //    //act-------------------------------------------------
+        //    var result = stringExtensions.ToCurrencyString(sut);
+
+        //    //assert--------------------------------------------
+        //    Assert.AreEqual(expected, result);
+        //}
+        #endregion
+
+    }
+}
